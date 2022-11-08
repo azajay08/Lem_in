d014dd5ad1f024bb6862ad303cef7da9c7ed8520
Miro-aleksi Tissari <miroaleksi.tissari@gmail.com>
2022-11-08 16:19:50 +0200

    Made a new file 'parser.c' which will contain the algorithm, and will be the home of the main solver/parser function

--- srcs/l_parser.c
+++ srcs/l_parser.c
@@ -1,30 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   l_main.c                                           :+:      :+:    :+:   */
+/*   l_parser.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtissari <mtissari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/13 00:19:09 by ajones            #+#    #+#             */
-/*   Updated: 2022/11/08 16:14:31 by mtissari         ###   ########.fr       */
+/*   Created: 2022/11/08 16:13:06 by mtissari          #+#    #+#             */
+/*   Updated: 2022/11/08 16:16:51 by mtissari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
-int	main(int ac, char **argv)
+void	parser(t_data *data)
 {
-	t_data	*data;
-
-	data = (t_data *)malloc(sizeof(t_data));
-	if (!data)
-		error_nothing_to_be_freed("MALLOC_FAIL");
-	init_data(data);
-	read_input(data);
-	// check error inputs and exit properly
-	// can also take the check to another fucntion
-	// read input if all is okay
-	parser(data);
-	//print the solution here
-	return (0);
+	// Use bfs and maybe other algorithms to get the best route
+	// print here or in the main?
+	// If in the main, we need to either return the string from here,
+	// or save it in data.
 }
