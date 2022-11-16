d85cedec4e5fa5d5d32f30ddb9df7d0ed5e8342a
miro <miroaleksi.tissari@gmail.com>
2022-11-16 15:30:09 +0200

    continuing with the calculator functions and solver functions

--- srcs/l_solver.c
+++ srcs/l_solver.c
@@ -26,36 +26,50 @@ t_option	*make_t_option(t_data *data, t_path *cur_path)
 	return (new_option);
 }
 
+int	calculate_paths(t_option *option)
+{
+	int	counter;
+
+	counter = 1;
+	while (option->next)
+		option = option->next;
+	while (option->previous)
+	{
+		option = option->previous;
+		counter++;
+	}
+	return (counter);
+}
+
 int	calculate_diff(t_option *option)
 {
 	int			diff;
 	t_option	*temp;
 
-	temp = option;
-	option = option->previous;
-	diff = temp->edges - option->edges;
+	temp = option->previous;
+	diff = option->edges - temp->edges;
+	// free (temp); // Not sure if it's a leak or not.. //
 	return (diff);
 }
 
-int	calculate_paths(t_data *data, t_option *option)
+int	calculate_paths_used(t_data *data, t_option *option)
 {
 	int	diff1;
 	int	diff2;
 	int	nb_of_paths;
 
-	nb_of_paths = 1;
+	nb_of_paths = calculate_paths(option);
 	if (option->next)
 	{
 		while (option->next)
-		{
 			option = option->next;
-			nb_of_paths++;
-		}
 		while(1)
 		{
 			diff2 = calculate_diff(option);
 			option = option->previous;
 			diff1 = calculate_diff(option);
+			if (diff2 == 0)
+				diff2 = -2;
 			if (data->nb_ants >= (diff1 + 3) + (diff2 + 2))
 				break ;
 			nb_of_paths--;
@@ -64,38 +78,45 @@ int	calculate_paths(t_data *data, t_option *option)
 	return (nb_of_paths);
 }
 
-int	calculate_turns(t_data *data, t_option *option)
-{
-	int	diff1;
-	int	diff2;
+/*
+	The "data->nb_ants >= (diff1 + 3) + (diff2 + 2)" works on 3 paths, but 
+	not sure if it works with more. We'll see later on.///
+*/
 
-	diff1 = option->edges
-	if ()
+int	send_ants(t_data *data, t_option *option)
+{
+	int	paths;
+
+	paths = calculate_paths(option);
+
 }
 
 t_option	*find_all_disjoint_paths(t_data *data)
 {
 	t_path		*cur_path;
-	t_option	*paths;
+	t_option	*all_paths;
 
+	all_paths = NULL;
 	while (1)
 	{
 		cur_path = bfs(data);
 		if (cur_path == NULL)
 			break ;
-		if (paths->start == NULL)
-			paths = make_t_option(data, cur_path);
+		if (all_paths->start == NULL)
+			all_paths = make_t_option(data, cur_path);
 		else
 		{
-			paths->next = make_t_option(data, cur_path);
-			paths->next->previous = paths;
-			paths = paths->next;
+			all_paths->next = make_t_option(data, cur_path);
+			all_paths->next->previous = all_paths;
+			all_paths = all_paths->next;
 		}
 		free (cur_path);
 	}
-	while (paths->previous != NULL)
-		paths = paths->previous;
-	return (paths);
+	if (all_paths == NULL)
+		(error_exit1("no paths found\n", data));
+	while (all_paths->previous != NULL)
+		all_paths = all_paths->previous;
+	return (all_paths);
 }
 
 void	solver(t_data *data)
@@ -106,8 +127,8 @@ void	solver(t_data *data)
 	if (data->nb_ants == 1)
 		return (bfs(data));//Just to show that it skips all funcitons after bfs.
 	orig_option = find_all_disjoint_paths(data);
-	if (calculate_paths(data, orig_option))
-		return (orig_option->start);
+	if (calculate_paths(orig_option) > calculate_paths_used(data, orig_option))
+		return (orig_option);
 	while (orig_option->turns >= next_added->turns)
 	{
 		free (orig_option);
